65639c21fbac7b1468d2aafc0a60a6dbf53ba88f
William Brehier <brehierwilliam@gmail.com>
2021-10-06 11:01:14 +0200

    first commit

---
+++ JS.js
@@ -0,0 +1,8 @@
+// function hover(){
+//     // console.log(document.getElementById("hover1"))
+//     // document.getElementById("hover1").innerHTML="<div class=\"maquette\"</div>";
+//     $('#hover1').append('<div class=\"maquette\"</div>');
+// }
+$(document).ready(function () {
+    $('#hover1').append('<div class=\"maquette\"</div>')
+  });
